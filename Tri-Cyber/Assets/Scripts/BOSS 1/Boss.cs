037aef0dda5ae4aad53d43286b4549b569f88d82
PEDRO HENRIQUE DOS SANTOS SILVA DIAS <20211170150042@ifrn.local>
2023-10-10 09:25:02 -0200

    Reprogramei

--- Tri-Cyber/Assets/Scripts/BOSS 1/Boss.cs
+++ Tri-Cyber/Assets/Scripts/BOSS 1/Boss.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
+    public float speed;
+    public float walkTime;
+    public bool walkRight;
+
+    public int health;
+    public int damage = 1;
+    
+    private float timer;
+    private Rigidbody2D rig;
+
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= walkTime)
+        {
+            walkRight = !walkRight;
+            timer = 0f;
+        }
+        if (walkRight)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            rig.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.left * speed;
+        }
+    }
+
+    public void Damage(int dmg)
+    {
+        health -= dmg;
+        anim.SetTrigger("protect");
+
+        if (health <=0)
+        {
+            anim.SetTrigger("dead");
+            Destroy(gameObject,1f);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //collision.gameObject.GetComponent<Player>().Damage(damage);
+        }
     }
 }
